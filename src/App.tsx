2a9418ee78280d0f44f2013a470343499f1402b8
CodeSandbox <support@codesandbox.io>
2025-07-04 06:59:30 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [count, setCount] = useState(0);
---
+++ src/navbar.tsx
@@ -0,0 +1,29 @@
+// NavbarComponent.jsx
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+
+function NavbarComponent() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">MyApp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar-nav" />
+        <Navbar.Collapse id="main-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <NavDropdown title="More" id="nav-dropdown">
+              <NavDropdown.Item href="#action1">Action 1</NavDropdown.Item>
+              <NavDropdown.Item href="#action2">Action 2</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
